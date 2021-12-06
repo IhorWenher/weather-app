5804e80a6a2e925859502e2e7562577b4570e37e
IhorVenher <73756717+IhorWenher@users.noreply.github.com>
2021-12-06 02:22:29 +0200

    added_styles

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/Material.css';
-import 'material-design-icons/iconfont/material-icons.css';
+//import 'material-design-icons/iconfont/material-icons.css';
 
 ReactDOM.render(
   <React.StrictMode>
